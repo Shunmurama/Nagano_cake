050772e2bfee45da28a17a8c48fb42b9ee43f1ca
EC2 Default User <ec2-user@ip-172-31-36-88.ap-northeast-1.compute.internal>
2023-08-14 17:10:28 +0000

    ログインレイアウト

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,13 +1,12 @@
 class Public::OrdersController < ApplicationController
   def new
-    @order = Order.new(order_params)
+    @order = Order.new
     @customers = Customer.all
     @customer = Customer.find(params[:id])
   end
 
   def confirm
     @order = Order.new
-    binding.pry
     @order_items = OrderItem.all
     @customers = Customer.all
     @tax = 1.1
@@ -17,10 +16,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.save
-      redirect_to orders_confirm_path
-    end
+    @order = current_customer.orders.new(order_params)
+      @cart_items = current_customer.cart_items.all
+      if @order.save
+         cart_items.each do |cart_item|
+           order_item = OrderItem.new
+           order_item.item_id = cart_item.item_id
+           order_item.order_id = @order.id
+           order_item.amount = cart_item.amount
+           order_item.purchase_price  = cart_item.item.price
+           order_item.save
+      end 
+         redirect_to orders_confirm_path
+    
   end
 
   def index
